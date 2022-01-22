182c097f6731ddeecb69493d6feadc560c5da960
DespicableMonkey <ballistic.minecrafter@gmail.com>
2022-01-21 20:19:18 -0600

    Add SpecialTank

--- src/main/java/frc/robot/commands/TankDriveCommand.java
+++ src/main/java/frc/robot/commands/TankDriveCommand.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 /** An example command that uses an example subsystem. */
 public class TankDriveCommand extends CommandBase {
   private final DriveBase driveBase;
-
+  
   public TankDriveCommand(DriveBase db) {
     this.driveBase = db;
     addRequirements(db);
@@ -21,16 +21,48 @@ public class TankDriveCommand extends CommandBase {
   public void initialize() {}
 
   public void execute() {
-      GenericHID gm = new GenericHID(1);
+      // GenericHID gm = new GenericHID(1);
+      // leftY: up down, rightX: left righ
+      double yAxis = RobotContainer.controller.getRawAxis(Constants.LEFT_Y_AXIS);
+      double xAxis = RobotContainer.controller.getRawAxis(Constants.RIGHT_X_AXIS);
 
-      double left = RobotContainer.controller.getRawAxis(Constants.LEFT_Y_AXIS);
-      double right = RobotContainer.controller.getRawAxis(Constants.RIGHT_Y_AXIS);
+      if(Math.abs(yAxis) < 0.01) yAxis = 0;
+      if(Math.abs(xAxis) < 0.01) xAxis = 0;
 
-      if(Math.abs(left) < 0.01) left = 0;
-      if(Math.abs(right) < 0.01) right = 0;
+    
+      if(Math.abs(xAxis) == 1)
+        yAxis *= .97;
+      double moveVector = Math.sqrt(yAxis * yAxis + xAxis + xAxis);
+      double fowardDifferential = yAxis / (yAxis + xAxis);
+      rotation(fowardDifferential * moveVector, Math.abs(1 - fowardDifferential * moveVector));
 
-      driveBase.drive(left, right);
+
+      // if(xAxis == 0 && yAxis != 0) {
+      //   tank(yAxis);
+      // } else if(xAxis != 0 && yAxis == 0) {
+      //   if(xAxis > 0) right(xAxis);
+      //   else left(xAxis);
+      // } else {
+      //   rotation(left, right);
+      // }
   }
+
+  public void tank(double axisweight) {
+    //>0  = foward, <0 = backward
+    driveBase.drive(axisweight, axisweight);
+  }
+  public void left(double axisweight) {
+    axisweight = Math.abs(axisweight);
+    driveBase.drive(-axisweight, axisweight);
+  }
+  public void right(double axisweight) {
+    axisweight = Math.abs(axisweight);
+    driveBase.drive(axisweight, -axisweight);
+  }
+  public void rotation(double left, double right) {
+    driveBase.drive(left, right);
+  }
+
 
   public void end(boolean interrupted) {}
 
