6bacd1138dd3075a1216c4d1ec49d9c7fb1f9562
DespicableMonkey <ballistic.minecrafter@gmail.com>
2022-03-12 16:29:51 -0600

    Add Subsystem Superclass

--- src/main/java/com/coyotronics/frc2022/subsystems/TransportSubsystem.java
+++ src/main/java/com/coyotronics/frc2022/subsystems/TransportSubsystem.java
@@ -6,27 +6,9 @@ import com.coyotronics.frc2022.Constants;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-public class TransportSubsystem extends SubsystemBase {
+public class TransportSubsystem extends ToggleSubsystem {
   
-  private CANSparkMax m_transportMotor;
-
-  public TransportSubsystem() {
-    m_transportMotor = new CANSparkMax(Constants.Shooter.kStorageMotor, MotorType.kBrushless);
-
-    m_transportMotor.restoreFactoryDefaults();
-  }
-  public void reset() {
-    this.m_transportMotor.set(0);
-  }
-  public void stop() {
-    this.reset();
-  }
-  public void runTransport(double speed) {
-    this.m_transportMotor.set(speed);
-  }
-
-  @Override
-  public void periodic() {}
-  @Override
-  public void simulationPeriodic() {}
+    public TransportSubsystem() {
+      super(Constants.Shooter.kStorageMotor, Constants.Shooter.kTransportSpeed);
+    }
 }
